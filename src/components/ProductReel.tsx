0cfea5963bad1b4f1fe88a0e23c21d989d950729
krish <114145459+krishkumar84@users.noreply.github.com>
2024-07-10 07:54:58 +0000

    api fix

--- src/components/ProductReel.tsx
+++ src/components/ProductReel.tsx
@@ -93,7 +93,7 @@ const ProductReel = (props: ProductReelProps) => {
             {map.map((product, i) => (
               <ProductListing
                 key={`product-${i}`}
-                product={product}
+                //  product={product}
                 index={i}
               />
             ))}
