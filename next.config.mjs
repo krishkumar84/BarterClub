1f92b43b922d11c6031bce0a8fba3cadebdbf53e
krish <114145459+krishkumar84@users.noreply.github.com>
2024-07-29 12:24:04 +0000

    cloudflare r2

--- next.config.mjs
+++ next.config.mjs
@@ -8,6 +8,18 @@ const nextConfig = {
           port: '',
           pathname: '/**',
         },
+        {
+          protocol: 'https',
+          hostname: 'pub-5b7aa644be4a4389bec851ef2147ddce.r2.dev',
+          port: '',
+          pathname: '/**',
+        },
+        {
+          protocol: 'https',
+          hostname: '4480569168e2c111c971804e36fe94c7.r2.cloudflarestorage.com',
+          port: '',
+          pathname: '/**',
+        },
       ],
     },
 };
--- src/app/api/upload/route.ts
+++ src/app/api/upload/route.ts
@@ -1,12 +1,16 @@
 import { NextRequest, NextResponse } from 'next/server';
-import { promises as fs } from 'fs';
-// import * as FTP from 'basic-ftp';
 import { randomBytes } from 'crypto';
-import path from 'path';
 import { getAuth } from "@clerk/nextjs/server";
+import { S3Client, PutObjectCommand } from '@aws-sdk/client-s3';
 
-
-// const ftpClient = new FTP.Client();
+const r2 = new S3Client({
+  region: "auto",
+  endpoint: process.env.URL ?? "",
+  credentials: {
+    accessKeyId: process.env.CLOUDFLARE_R2_ACCESS_KEY_ID ?? " ",
+    secretAccessKey: process.env.CLOUDFLARE_R2_SECRET_ACCESS_KEY ?? "",
+  },
+});
 
 export async function POST(req:any) {
   try {
@@ -23,30 +27,28 @@ export async function POST(req:any) {
       return NextResponse.json({ error: 'No file uploaded' }, { status: 400 });
     }
 
-    // const tempFilePath = path.join('/tmp', file.name); // Temporary file path
     const buffer = Buffer.from(await file.arrayBuffer());
     const fileExtension = data.get('file').name.split('.').pop();
     const uniqueString = randomBytes(4).toString('hex'); // Generate a random 8-character string
     const fileName = `${uniqueString}.${fileExtension}`;
-
-    const tempFilePath = path.join('/tmp', fileName);
-    await fs.writeFile(tempFilePath, buffer);
-
-    const publicDir = path.resolve('./public/uploads');
-    const publicFilePath = path.join(publicDir, fileName);
-    await fs.mkdir(publicDir, { recursive: true });
-
-    // await fs.mkdir(uploadDir, { recursive: true });
-    // await fs.writeFile(filePath, buffer);
-    await fs.copyFile(tempFilePath, publicFilePath);
-
-    // Delete the original file in /tmp
-    await fs.unlink(tempFilePath);
-
-    const fileUrl = `/uploads/${fileName}`; // URL to access the uploaded file
-    return NextResponse.json({ message: 'File uploaded successfully', url: fileUrl });
+    console.log(file.type);
+    const putObjectCommand = new PutObjectCommand({
+      Bucket: process.env.CLOUDFLARE_R2_BUCKET_NAME,
+      Key: fileName,
+      Body: buffer,
+      ContentType: file.type,
+      ACL: 'public-read',
+    });
+    try {
+      const response = await r2.send(putObjectCommand);
+      const fileUrl = `https://pub-5b7aa644be4a4389bec851ef2147ddce.r2.dev/${fileName}`; 
+       return NextResponse.json({ message: 'File uploaded successfully', url: fileUrl });
+    } catch (error) {
+      console. log ("error:", error);
+      return NextResponse. json({ success: false }, { status: 500 });
+      }
   } catch (error) {
-    console.error('FTP upload error:', error);
+    console.error('upload error:', error);
     return NextResponse.json({ error: 'Error uploading file' }, { status: 500 });
   }
 }
