8b28580c7efceceacabd1bcd0e4532cbc75231eb
krish <114145459+krishkumar84@users.noreply.github.com>
2024-07-29 09:43:40 +0000

    deploy err solve

--- src/app/api/upload/route.ts
+++ src/app/api/upload/route.ts
@@ -29,36 +29,22 @@ export async function POST(req:any) {
     const uniqueString = randomBytes(4).toString('hex'); // Generate a random 8-character string
     const fileName = `${uniqueString}.${fileExtension}`;
 
-    const uploadDir = path.resolve('./public/uploads');
-    const filePath = path.join(uploadDir, fileName);
+    const tempFilePath = path.join('/tmp', fileName);
+    await fs.writeFile(tempFilePath, buffer);
 
-    await fs.mkdir(uploadDir, { recursive: true });
-    await fs.writeFile(filePath, buffer);
+    const publicDir = path.resolve('./public/uploads');
+    const publicFilePath = path.join(publicDir, fileName);
+    await fs.mkdir(publicDir, { recursive: true });
+
+    // await fs.mkdir(uploadDir, { recursive: true });
+    // await fs.writeFile(filePath, buffer);
+    await fs.copyFile(tempFilePath, publicFilePath);
+
+    // Delete the original file in /tmp
+    await fs.unlink(tempFilePath);
 
     const fileUrl = `/uploads/${fileName}`; // URL to access the uploaded file
     return NextResponse.json({ message: 'File uploaded successfully', url: fileUrl });
-
-    // Write the file to the temporary location
-    // const arrayBuffer = await file.arrayBuffer();
-    // const buffer = Buffer.from(arrayBuffer);
-    // await fs.writeFile(tempFilePath, buffer);
-
-    // await ftpClient.access({
-    //   host: "46.28.46.155", // Use your domain here
-    //   user: "u166069102.Krish",
-    //   password: "BarterClub&2611Z",
-    //   port: 21,
-    //   secure: true,
-    //   secureOptions: { rejectUnauthorized: false }
-    // });
-
-    // // Upload the file from the temporary location
-    // await ftpClient.uploadFrom(createReadStream(tempFilePath), `/upload/${file.name}`);
-
-    // // Clean up the temporary file
-    // await fs.unlink(tempFilePath);
-
-    // return NextResponse.json({ message: 'File uploaded successfully' }, { status: 200 });
   } catch (error) {
     console.error('FTP upload error:', error);
     return NextResponse.json({ error: 'Error uploading file' }, { status: 500 });
